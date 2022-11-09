cccefdc75ec23923605ee27c734e5fbce2a479fb
Matthew Ballard <matt@mrb.email>
2022-11-09 08:57:52 -0500

    add quiz10

---
+++ 05/Quiz5.lean
@@ -0,0 +1,11 @@
+-- Quiz 5
+variable (A B : Prop)
+
+-- Problem 1 
+example (h : A ∧ B) : B ∧ A := sorry 
+
+-- Problem 2
+example (h : A → B) : ¬ A ∨ B := sorry 
+
+-- Problem 3 
+example (h : ¬ A ∨ B) : A → B := sorry 
