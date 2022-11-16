759591ce358ca5e7ba947b3e18969dada81b9a10
lizziestephenson <111578014+lizziestephenson@users.noreply.github.com>
2022-11-16 22:39:16 +0000

    Quiz5-LizzieStephenson

--- 05/Quiz5.lean
+++ 05/Quiz5.lean
@@ -2,10 +2,27 @@
 variable (A B : Prop)
 
 -- Problem 1 
-example (h : A ∧ B) : B ∧ A := sorry 
+example (h : A ∧ B) : B ∧ A := by
+  exact And.intro (And.right h) (And.left h)
 
 -- Problem 2
-example (h : A → B) : ¬ A ∨ B := sorry 
+example (h : A → B) : ¬ A ∨ B := by
+  
 
 -- Problem 3 
-example (h : ¬ A ∨ B) : A → B := sorry 
+example (h : ¬ A ∨ B) : A → B := by
+  apply Or.elim h
+  intro h₁ 
+  intro h₂
+  exact Or.right h
+  intro h₁
+  intro h₂
+  exact h₁
+
+
+
+
+
+
+  
+
