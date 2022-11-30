acfe685882c5fc923263119f383f425b61b03df3
lizziestephenson <111578014+lizziestephenson@users.noreply.github.com>
2022-11-30 14:14:43 +0000

    Quiz5-LizzieS

--- 05/Quiz5.lean
+++ 05/Quiz5.lean
@@ -7,17 +7,19 @@ example (h : A ∧ B) : B ∧ A := by
 
 -- Problem 2
 example (h : A → B) : ¬ A ∨ B := by
-  
+   
+
 
 -- Problem 3 
 example (h : ¬ A ∨ B) : A → B := by
   apply Or.elim h
-  intro h₁ 
-  intro h₂
-  exact Or.right h
-  intro h₁
-  intro h₂
-  exact h₁
+  intro h₁ h₂ 
+  have b : False := h₁ h₂
+  exact False.elim b 
+  intro b _
+  exact b 
+
+
 
 
 
